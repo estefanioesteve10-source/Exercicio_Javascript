d374fec07f008ab5a66c3da9592ad005f1ef3216
Estefanio Fernando <estefanioesteve10@gmail.com>
2026-01-08 23:03:24 +0000

    Dois primeiros exercicios

---
+++ ex1.js
@@ -0,0 +1,5 @@
+let note1 = 16;
+let note2 = 17;
+var average = (note1 + note2) / 2;
+console.log("This is the average: " + average);
+console.log("Congratulations!");
---
+++ ex2.js
@@ -0,0 +1,29 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const peers = [];
+const nonPeers = [];
+
+for (let i = 0; i < numbers.length; i++){
+    if (numbers[i]%2 === 0)
+    {
+       peers.push(numbers[i]);
+    }
+    else
+    {
+        nonPeers.push(numbers[i]);
+    }
+
+}
+console.log("These are the even numbers: " + peers);
+console.log("These are the odd numbers: " + nonPeers);
+
+let sumPeers = 0;
+for (let i = 0; i < peers.length; i++){
+    sumPeers += peers[i]; 
+}
+console.log("The sum of the even numbers is: " + sumPeers);
+
+let sumnonPeers = 0;
+for (let i = 0; i < nonPeers.length; i++){
+    sumnonPeers += nonPeers[i]; 
+}
+console.log("The sum of the odd numbers is: " + sumnonPeers);
